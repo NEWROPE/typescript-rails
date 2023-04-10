c1a0897cb642e29810b89296c162a7a1303c5149
Mason Louchart <mason@sekaijuu.de>
2023-04-10 19:13:17 +0900

    Fix syntax warnings

--- test/template_handler_test.rb
+++ test/template_handler_test.rb
@@ -31,25 +31,25 @@ class TemplateHandlerTest < ActiveSupport::TestCase
 
   test 'typescript views are served as javascript' do
     get '/site/index.js'
-    assert_match /var x = 5;\s*/,
+    assert_match (/var x = 5;\s*/),
         source
   end
 
   test '<reference> to other .ts file works' do
     get '/site/ref1_2.js'
-    assert_match /var f = function \(x, y\) \{\s*return x \+ y;\s*\};\s*f\(1, 2\);\s*/,
+    assert_match (/var f = function \(x, y\) \{\s*return x \+ y;\s*\};\s*f\(1, 2\);\s*/),
         source
   end
 
   test '<reference> to other .d.ts file works' do
     get '/site/ref2_2.js'
-    assert_match /f\(1, 2\);\s*/,
+    assert_match (/f\(1, 2\);\s*/),
         source
   end
 
   test '<reference> to multiple .ts files works' do
     get '/site/ref3_1.js'
-    assert_match /var f1 = function \(\) \{\s*\};\s*var f2 = function \(\) \{\s*\};\s*f1\(\);\s*f2\(\);/,
+    assert_match (/var f1 = function \(\) \{\s*\};\s*var f2 = function \(\) \{\s*\};\s*f1\(\);\s*f2\(\);/),
         source
   end
 
